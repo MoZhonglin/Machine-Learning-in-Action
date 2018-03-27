461c3feddffa22413e7e9d0d005a8ec1aee16f04
MoZhonglin <frank.mo@live.com>
2018-03-27 21:20:19 +0800

    add chapter_2

---
+++ chapter_2/dataVisual.py
@@ -0,0 +1,11 @@
+import kNN
+from numpy import *
+import matplotlib
+import matplotlib.pyplot as plt
+
+datingDataMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(datingDataMat[:, 0], datingDataMat[: ,1], 15.0*array(datingLabels), 15.0*array(datingLabels))
+plt.show()
